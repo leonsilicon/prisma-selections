6a8b7159f5f32022123769fa9fbfba5b44b12bb3
Leon Si <leon@leondreamed.com>
2023-01-15 11:04:52 -0500

    feat: comimt

---
+++ packages/prisma-selections/src/index.ts
@@ -0,0 +1,14 @@
+export type {
+	ExpandMapping,
+	GetPayload,
+	InferSelectionDefinition,
+	RecursivelyExpandSelection,
+	SelectionContext,
+	SelectionDefinition,
+	WithOptions,
+} from './types/selections.js'
+export {
+	createSelectionFunction,
+	defineSelectionMappings,
+	expandSelections,
+} from './utils/selections.js'
---
+++ packages/prisma-selections/src/types/selections.ts
@@ -0,0 +1,64 @@
+import type { PrismaClient } from '@prisma/client'
+import type { UnionToIntersection } from 'type-fest'
+
+export interface SelectionDefinition<
+	_PrismaSelect,
+	_SelectionMappings extends Record<`$${string}`, unknown>
+> extends Record<`$${string}`, unknown> {}
+
+export type InferSelectionDefinition<
+	SelectionDefinitionGetter extends (...args: any) => any
+> = Awaited<ReturnType<SelectionDefinitionGetter>>
+
+export type ExpandMapping<SelectionMappings, Options> = {
+	[OptionKey in keyof Options]: OptionKey extends keyof SelectionMappings
+		? ExpandMapping<SelectionMappings, SelectionMappings[OptionKey]>
+		: Record<OptionKey, Options[OptionKey]>
+}[keyof Options]
+
+export interface GetPayload<
+	Definition extends SelectionDefinition<any, any>,
+	Options
+> {
+	select: Definition extends SelectionDefinition<
+		any,
+		infer SelectionMappings
+	>
+		? UnionToIntersection<ExpandMapping<SelectionMappings, Options>>
+		: never
+}
+
+export type WithOptions<
+	Definition extends SelectionDefinition<any, any>,
+	Select
+> = Definition extends SelectionDefinition<
+	any,
+	infer SelectionMappings
+>
+	? Select & {
+			[Key in keyof SelectionMappings]?: boolean
+	  }
+	: never
+
+type Selections<SelectionMappingObject> = keyof {
+	[K in keyof SelectionMappingObject as K extends `$${string}`
+		? K
+		: never]: true
+}
+
+export type RecursivelyExpandSelection<
+	SelectionMapping,
+	SelectionMappingObject
+> = Omit<SelectionMappingObject, `$${string}`> &
+	(Selections<SelectionMappingObject> extends never
+		? // eslint-disable-next-line @typescript-eslint/ban-types -- Cannot use `EmptyObject` here or else it messes up Prisma's type inference (and we intersect with it anyways)
+		  {}
+		: {
+				[K in Selections<SelectionMappingObject>]: K extends keyof SelectionMapping
+					? SelectionMapping[K]
+					: never
+		  }[Selections<SelectionMappingObject>])
+
+export interface SelectionContext {
+	prisma: PrismaClient
+}
